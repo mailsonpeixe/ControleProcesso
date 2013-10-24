f800b84736f0abcfb5db1003f4584a6d1027753e
Gustavo <Gustavo@Gustavo-PC>
2013-10-24 19:00:12 -0200

    rotas

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -37,3 +37,19 @@ App.config(['$routeProvider', function($routeProvider) {
         redirectTo: '/home'
     });
 }]);
+
+App.directive('jqdatepicker', function () {
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+         link: function (scope, element, attrs, ngModelCtrl) {
+            element.datepicker({
+                dateFormat: 'DD, d  MM, yy',
+                onSelect: function (date) {
+                    scope.date = date;
+                    scope.$apply();
+                }
+            });
+        }
+    };
+});
