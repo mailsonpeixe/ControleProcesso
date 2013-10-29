bb64b4b5d3d5e0f7b152bad82eaf025ff5e89f20
Gustavo <Gustavo@Gustavo-PC>
2013-10-29 20:17:57 -0200

    rotas

---
+++ src/main/webapp/resources/js/controllers/IndexController.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * CarController
+ * @constructor
+ */
+var IndexController = function($scope, $http, $routeParams) {
+    $scope.date = new Date();
+    $scope.cpf = '07257712913';
+   
+}
